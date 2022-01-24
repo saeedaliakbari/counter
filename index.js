ddcada864ba35321df31e63d59140b110e9ccb76
Saeed <saliakbari6603@gmail.com>
2022-01-24 22:55:41 +0330

    improve code
    -use class list  and contain

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const incBtn = document.querySelector(".increment");
-const decBtn = document.querySelector(".decrement");
-const resetBtn = document.querySelector(".reset");
+// const incBtn = document.querySelector(".increment");
+// const decBtn = document.querySelector(".decrement");
+// const resetBtn = document.querySelector(".reset");
 const counterValue = document.querySelector(".counter span");
+const btns = document.querySelectorAll(".btn");
+let count = 0;
+// incBtn.addEventListener("click", () => {
+//   counterValue.textContent = parseInt(counterValue.textContent) + 1;
+// });
 
-incBtn.addEventListener("click", () => {
-  counterValue.textContent = parseInt(counterValue.textContent) + 1;
+// decBtn.addEventListener("click", () => {
+//   counterValue.textContent = parseInt(counterValue.textContent) - 1;
+// });
+
+// resetBtn.addEventListener("click", () => (counterValue.textContent = "0"));
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.classList.contains("increment")) count++;
+    else if (btn.classList.contains("decrement")) count--;
+    else count = 0;
+
+    if (count > 0) counterValue.style.color = "green";
+    else counterValue.style.color = "red";
+
+    counterValue.textContent = count;
+  });
 });
-
-decBtn.addEventListener("click", () => {
-  counterValue.textContent = parseInt(counterValue.textContent) - 1;
-});
-
-resetBtn.addEventListener("click", () => (counterValue.textContent = "0"));
